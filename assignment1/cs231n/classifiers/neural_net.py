db572660ac604ba210ee1dd0775d90021cafbf20
Navarre Ginsberg <nginsberg@wesleyan.edu>
2017-04-22 13:07:42 -0700

    Finished A1

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -97,14 +97,14 @@ class TwoLayerNet(object):
     # and biases. Store the results in the grads dictionary. For example,       #
     # grads['W1'] should store the gradient on W1, and be a matrix of same size #
     #############################################################################
-    scoresGrad = normalized
+    scoresGrad = normalized.copy()
     scoresGrad[np.arange(scoresGrad.shape[0]), y] -= 1
     grads['b2'] = np.mean(scoresGrad, axis=0)
-    grads['W2'] = h.T.dot(scoresGrad) + reg * W2
+    grads['W2'] = 1.0/N * h.T.dot(scoresGrad) + reg * W2
     dh = scoresGrad.dot(W2.T)
     dh[h == 0] = 0
     grads['b1'] = np.mean(dh, axis=0)
-    grads['W1'] = X.T.dot(dh) + reg * W1
+    grads['W1'] = 1.0/N * X.T.dot(dh) + reg * W1
     #############################################################################
     #                              END OF YOUR CODE                             #
     #############################################################################
